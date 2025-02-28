a6c552e1c2357097b003cca26c151503be5ea0cf
ALUMNO PC7 <ALUMNO PC7@LABCOMP3PC7>
2025-02-28 10:34:43 -0600

    Agregar archivos de proyecto.

---
+++ Parcial1_Caida_D_Dado/Controllers/DadoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Parcial1_Caida_D_Dado.Models;
+
+namespace Parcial1_Caida_D_Dado.Controllers
+{
+    public class DadoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calcular(CaidaModel obdado)
+        {
+            Random rnd = new Random();
+            obdado.NumeroGenerado = rnd.Next(1, 50);
+            return View("Resultado", obdado);
+        }
+    }
+}
---
+++ Parcial1_Caida_D_Dado/Models/CaidaModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Parcial1_Caida_D_Dado.Models
+{
+    public class CaidaModel
+    {
+        private static Random random = new Random();
+        public double TiempoCaida { get; set; }
+        public int NumeroGenerado { get; set; }
+        public double Altura { get; set; }
+    }
+}
